3b5373858419d0555e1bc26d5818c25f1e400d3f
Tanise Santos <tanise234@gmail.com>
2022-04-26 23:31:32 -0300

    Req 1 iniciado

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,15 @@
+const { consoleLog } = require('mocha/lib/reporters/base');
+
 const fetchProducts = () => {
   // seu código aqui
+  const f = fetch(
+    'https://api.mercadolibre.com/sites/MLB/search?q=$computador',
+  )
+  .then((response) => response.json())
+  .then((data) => consoleLog(data));
 };
+
+// fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
