56f69d7e248033788a0e2164a42b434dbb50e496
Tanise Santos <tanise234@gmail.com>
2022-04-29 20:26:14 -0300

    Requisito 8 iniciado

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,12 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
+  test('1.1 - Teste se fetchProducts é uma função', async () => {
+  await expect(typeof(fetchProducts)).toBe('function');
+  });
+  // test('1.2 - Teste se fetch foi chamada quando fetchProducts("computador") for executada', async () => {
+  //   await fetchProducts('computador');
+  //   expect(typeof(fetchProducts)).toBe('function');
+  //   });
   fail('Teste vazio');
 });
